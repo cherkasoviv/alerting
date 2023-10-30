528ac9175930e28f0a1dc666d51212bdb853909b
Черкасов Игорь Владимирович <cherkasov.i@delotech.ru>
2023-10-31 00:27:39 +0400

    added hash check on server

--- internal/server/middleware/hash/mw_hash.go
+++ internal/server/middleware/hash/mw_hash.go
@@ -7,7 +7,9 @@ import (
 func Hash256Middleware(key string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		fn := func(w http.ResponseWriter, r *http.Request) {
+			ow := w
 
+			next.ServeHTTP(ow, r)
 		}
 
 		return http.HandlerFunc(fn)
